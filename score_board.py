d0b31b4b0a7089ea0082b906c130b4bbab1d78cc
kurta17 <114169244+kurta17@users.noreply.github.com>
2023-10-10 11:33:13 +0700

    add score board

---
+++ score_board.py
@@ -0,0 +1,21 @@
+import turtle
+
+class ScoreBoard(turtle.Turtle):
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.color("white")
+        self.penup()
+        self.hideturtle()
+        self.goto(0, 260)
+        self.print_score()
+        
+
+    def print_score(self):
+        self.clear()  
+        self.write(f"Score: {self.score}", align="center", font=("Arial", 24, "normal"))
+
+    def increase_score(self):
+        self.score += 1
+        self.print_score()
+
