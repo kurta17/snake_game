968c0e29e17bb72698657ded61ecf5261795f08f
kurta17 <114169244+kurta17@users.noreply.github.com>
2023-10-10 11:00:50 +0700

    add food's move random position

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ from turtle import Screen
 import turtle as t
 import time
 from snake import Snake
+from food import Food
 #screen 
 screen = Screen()
+screen.title("Snake Game")
 screen.bgcolor("black")
 screen.setup(width=600,height=600)
 
@@ -13,15 +15,22 @@ screen.tracer(0)
 #move snake
 snake = Snake()
 screen.listen()
+
 screen.onkey(snake.up, key="Up") 
 screen.onkey(snake.left, key="Left") 
 screen.onkey(snake.right, key="Right") 
 screen.onkey(snake.down, key="Down") 
 
+#food
+
+food = Food()
 while True: 
-    time.sleep(0.25)
+    time.sleep(0.1)
     snake.move()
+    if snake.head.distance(food)< 20:
+        food.food_pos()
     screen.update()
+    
 
 
 
